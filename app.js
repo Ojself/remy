783f2401551c356ab60af63e63f90ce95a2bd8bd
Tormod Flesjø <tormod.flesjo@gmail.com>
2019-05-10 20:19:59 +0200

    minor changes to app.js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 /* Why are you guys looking into my code. You should be working on your project;) */
 
 const claimPrice = () => {
+  let x = '';
   for (;;) {
-    alert('Hello world!');
+    x += 'x';
+    console.log(x);
   }
 };
